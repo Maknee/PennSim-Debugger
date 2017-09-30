0fe9be16231e8ddf3a9e563fb3d856f9af594d75
Xuerui <faxuerui@seas.upenn.edu>
2017-09-30 14:23:24 -0400

    Added test case for Henry

--- test.asm
+++ test.asm
@@ -1,46 +1,29 @@
-CONST R1, 1
-CONST R2, -1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;main;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		.CODE
+		.FALIGN
+main
+	;; prologue
+	STR R7, R6, #-2	;; save return address
+	STR R5, R6, #-3	;; save base pointer
+	ADD R6, R6, #-3
+	ADD R5, R6, #0
+	ADD R6, R6, #-3	;; allocate stack space for local variables
+	;; function body
+	CONST R7, #5
+	STR R7, R5, #-1
+	CONST R7, #-10
+	STR R7, R5, #-2
+	LDR R7, R5, #-2
+	LDR R3, R5, #-1
+	DIV R7, R7, R3
+	STR R7, R5, #-3
+	CONST R7, #0
+L1_test
+	;; epilogue
+	ADD R6, R5, #0	;; pop locals off stack
+	ADD R6, R6, #3	;; free space for return address, base pointer, and return value
+	STR R7, R6, #-1	;; store return value
+	LDR R5, R6, #-3	;; restore base pointer
+	LDR R7, R6, #-2	;; restore return address
+	RET
 
-CMP R1, R2
-BRn FAIL
-
-CONST R1, -1
-CONST R2, 1
-
-CMP R1, R2
-BRz FAIL
-
-CONST R1, -1
-CONST R2, 1
-
-CMP R1, R2
-BRp FAIL
-
-CONST R1, 1
-CONST R2, -1
-
-CMP R1, R2
-BRnz FAIL
-
-CONST R1, -1
-CONST R2, 1
-
-CMP R1, R2
-BRzp FAIL
-
-CONST R1, 1
-CONST R2, 1
-
-CMP R1, R2
-BRnp FAIL
-
-CONST R1, 1
-CONST R2, 1
-
-CMP R1, R2
-BRnzp SUCCESS
-
-SUCCESS:
-	NOP
-FAIL:
-	NOP
